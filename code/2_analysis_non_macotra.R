d3dbe6cd97ce3c84bfa66701682502ee5479831c
gwenknight <gwen.knight@github.com>
2021-02-08 20:04:37 +0000

    Making Figure 2 for paper

--- code/2_analysis_non_macotra.R
+++ code/2_analysis_non_macotra.R
@@ -26,11 +26,56 @@ ggplot(param %>% filter(strain_name %in% strains), aes(x= inocl, y = timepeak, g
   facet_wrap(~strain_name, nrow = 2) + theme(legend.position = "none") + 
   scale_x_continuous(breaks = seq(2:7), labels = function(x) parse(text=paste("10^",x)),"Inoculum", limits = c(1.5,6.5)) + 
   scale_y_continuous(expression(paste("Time to first peak (", italic(tmax),")")))
-ggsave("plots/time_peak_vs_inoculum_strain_col.pdf", width = 12)
+ggsave("plots/final/time_peak_vs_inoculum_strain_col.pdf", width = 12)
 
 
-ggplot(param %>% filter(strain_name %in% strains), aes(x = inocl, y = timepeak, group = inocl)) + 
+ga <- ggplot(param %>% filter(strain_name %in% strains), aes(x = inocl, y = timepeak, group = inocl)) + 
   geom_boxplot() + 
   scale_x_continuous(breaks = seq(2:7), labels = function(x) parse(text=paste("10^",x)),"Inoculum", limits = c(1.5,6.5)) + 
   scale_y_continuous(expression(paste("Time to first peak (", italic(tmax),")")))
-ggsave("plots/time_peak_vs_inoculum.pdf")
+ggsave("plots/final/time_peak_vs_inoculum.pdf")
+
+
+## Data
+data_od <- read_csv("proof_of_principle/ddm_OD.csv")[,-1]
+data_cs <- read_csv("proof_of_principle/ddm_CS.csv")[,-1]
+
+
+### Look at data
+g1 <- ggplot(data_cs %>% filter(strain == "SA2704"), aes(x=Time, y = value_J, group = interaction(rep, inoc))) + 
+  geom_line(aes(col = factor(inoc))) + 
+  facet_wrap(~strain) + 
+  scale_y_continuous("Heat curve") +
+  scale_x_continuous(lim = c(0,25)) + 
+  scale_color_discrete("Inoculum")
+
+### Smooth OD values
+data_od <- data_od %>% group_by(rep, exp, variable, strain, inoc) %>%
+  mutate(ma_value = rollapply(value, 10, mean,fill = NA),
+         differ = c(0,diff(ma_value))) %>%
+  ungroup()
+
+g2 <- ggplot(data_od %>% filter(strain == "SA2704"), aes(x=Time, y = ma_value, group = interaction(rep, inoc))) + 
+  geom_line(aes(col = factor(inoc))) + 
+  facet_wrap(~strain) + 
+  scale_y_continuous("OD")+ 
+  scale_color_discrete("Inoculum")
+
+g3 <- ggplot(data_od %>% filter(strain == "SA2704"), aes(x=Time, y = differ, group = interaction(rep, inoc))) + 
+  geom_line(aes(col = factor(inoc))) + 
+  facet_wrap(~strain) + 
+  scale_y_continuous("OD")+ 
+  scale_color_discrete("Inoculum")
+ggsave("proof_of_principle/OD_data_difference.pdf")
+
+### Cumulate heat flow values
+g4 <- ggplot(data_cs %>% filter(strain == "SA2704"), aes(x=Time, y = csum, group = interaction(rep, inoc))) + 
+  geom_line(aes(col = factor(inoc))) + 
+  facet_wrap(~strain) + 
+  scale_y_continuous("Heat curve") + 
+  scale_x_continuous(lim = c(0,25)) + 
+  scale_color_discrete("Inoculum") 
+
+gb <- ((g1 + g4) / (g3 + g2)) + plot_layout(guides = 'collect') + plot_annotation(tag_levels = 'A') & theme(legend.position='bottom')
+gb |  ga  
+ggsave("plots/final/linear_OD_vs_CS.pdf",width = 15, height = 10)
